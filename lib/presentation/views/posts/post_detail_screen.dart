f1ec59148c42b589ab00ef51815d3186c479a09e
Mauricio <maujpok@gmail.com>
2024-09-17 14:54:00 -0300

    added post detail screen

--- lib/presentation/views/posts/post_detail_screen.dart
+++ lib/presentation/views/posts/post_detail_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:fudo/app/ui_style.dart';
+import 'package:fudo/data/models/post_model.dart';
+import 'package:gap/gap.dart';
 
 class PostDetailScreen extends StatelessWidget {
-  const PostDetailScreen({super.key});
+  const PostDetailScreen({super.key, required this.post});
+  final PostModel post;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    const textStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: AppSizes.textMax);
+
+    return Scaffold(
+      
+      appBar: AppBar(
+        
+        title: const Text('Post Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(AppSizes.paddingDefault),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Post Title'),
+            Text(post.title ?? '', style: textStyle),
+            const Gap(AppSizes.paddingDefault),
+            const Text('Post Body'),
+            Text(post.body ?? '', style: textStyle),
+            const Gap(AppSizes.paddingDefault),
+            const Text('Post Id'),
+            Text(post.id.toString(), style: textStyle),
+            const Gap(AppSizes.paddingDefault),
+            const Text('From User'),
+            Text(post.userId.toString(), style: textStyle),
+          ],
+        ),
+      ),
+    );
   }
 }
