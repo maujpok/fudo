8d501bec1c29185aa588157accc2c9d456aa2312
Mauricio <maujpok@gmail.com>
2024-09-17 14:54:18 -0300

    added post list screen

--- lib/presentation/views/posts/post_list_screen.dart
+++ lib/presentation/views/posts/post_list_screen.dart
@@ -1,18 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fudo/app/extensions.dart';
+import 'package:fudo/app/routes.dart';
 import 'package:fudo/app/ui_style.dart';
+import 'package:fudo/presentation/utils/toast_handler.dart';
+import 'package:fudo/presentation/views/posts/bloc/posts_bloc.dart';
+import 'package:fudo/presentation/views/posts/widgets/post_widget.dart';
+import 'package:gap/gap.dart';
 
-class PostListScreen extends StatelessWidget {
+class PostListScreen extends StatefulWidget {
   const PostListScreen({super.key});
 
   @override
+  State<PostListScreen> createState() => _PostListScreenState();
+}
+
+class _PostListScreenState extends State<PostListScreen> {
+  bool _showExitWarning = true;
+
+  @override
+  void initState() {
+    if (context.read<PostsBloc>().state.postList == null) {
+      context.read<PostsBloc>().add(GetPostsEvent());
+    }
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.secondaryColor,
-      appBar: AppBar(
-        backgroundColor: AppColors.secondaryColor,
-        title: const Text('Posts'),
-      ),
-      body: Container(),
+    return PopScope(
+      canPop: !_showExitWarning,
+      onPopInvoked: (didPop) async {
+        if (_showExitWarning) {
+          ToastHandler.showWarningToast('Press back again to exit');
+          setState(() {
+            _showExitWarning = false;
+          });
+        }
+      },
+      child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: AppColors.primaryColor,
+            foregroundColor: AppColors.secondaryColor,
+            onPressed: () => context.pushNamed(RoutePath.postCreate),
+            child: const Icon(Icons.add),
+          ),
+          appBar: AppBar(
+            title: const Text('Posts'),
+          ),
+          body: BlocBuilder<PostsBloc, PostsState>(
+            builder: (context, state) {
+              if (state.loading) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: AppColors.primaryColor,
+                    strokeWidth: 1.2,
+                  ),
+                );
+              }
+              if (state.postList != null && (state.postList ?? []).isEmpty) {
+                return const Center(
+                  child: Text('No Posts'),
+                );
+              }
+
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(AppSizes.paddingMin),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search posts...',
+                        prefixIcon: const Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(AppSizes.borderRadiusDefault),
+                        ),
+                      ),
+                      onChanged: (query) async {},
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.separated(
+                      separatorBuilder: (context, index) => const Gap(AppSizes.paddingMin),
+                      itemCount: (state.postList ?? []).length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        final post = state.postList?[index];
+                        return PostWidget(post: post);
+                      },
+                    ),
+                  ),
+                ],
+              );
+            },
+          )),
     );
   }
 }
---
+++ lib/presentation/views/posts/widgets/post_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:fudo/app/extensions.dart';
+import 'package:fudo/app/routes.dart';
+import 'package:fudo/data/models/post_model.dart';
+
+class PostWidget extends StatelessWidget {
+  const PostWidget({
+    super.key,
+    required this.post,
+  });
+
+  final PostModel? post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () => context.pushNamed(
+          RoutePath.postDetail,
+          arguments: post,
+        ),
+        title: Text(post?.title ?? ''),
+        subtitle: Text(
+          post?.body ?? '',
+          maxLines: 2,
+          overflow: TextOverflow.fade,
+        ),
+        trailing: const Icon(Icons.arrow_forward_ios_rounded),
+      ),
+    );
+  }
+}
